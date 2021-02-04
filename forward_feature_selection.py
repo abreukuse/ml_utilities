64a8a9ba9361d213048d394d0357c9acacecec13
abreukuse <t.kuse6@gmail.com>
2021-02-04 11:47:35 -0300

    Fixed accepted variables issue.

--- forward_feature_selection.py
+++ forward_feature_selection.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-def foward_feature_selection(X_train, 
+def forward_feature_selection(X_train, 
                              X_test, 
                              y_train, 
                              y_test, 
@@ -9,8 +9,8 @@ def foward_feature_selection(X_train,
                              analyse_together=None,
                              steps=20,
                              label=1):
-    aceitas = []
-    valor_maior_score = 0
+    accepted = []
+    greater_score = 0
 
     if analyse_together:
         analyse_together_flatten = [item_alone for items_together in analyse_together for item_alone in items_together]
@@ -20,13 +20,13 @@ def foward_feature_selection(X_train,
         variables = [[item] for item in X_train.columns]
 
     for step in range(steps):
-        var_maior_score = None
+        variable_greater_score = None
         for variable in variables:
-            if variable in aceitas:
+            if variable[0] in accepted:
                 continue
          
-            model.fit(X_train[aceitas + variable], y_train)
-            y_pred = model.predict(X_test[aceitas + variable])
+            model.fit(X_train[accepted + variable], y_train)
+            y_pred = model.predict(X_test[accepted + variable])
 
             scores = {'accuracy_score': accuracy_score,
                       'precision_score': precision_score,
@@ -37,19 +37,19 @@ def foward_feature_selection(X_train,
 
             score = scores[metric](**parameters)
 
-            if score > valor_maior_score:
-                var_maior_score = variable
-                valor_maior_score = score
+            if score > greater_score:
+                variable_greater_score = variable
+                greater_score = score
 
-        if var_maior_score is None:
+        if variable_greater_score is None:
             break
 
-        aceitas = [[item] for item in aceitas]
-        aceitas.append(var_maior_score)
-        aceitas = [y for x in aceitas for y in x] # flatten
+        accepted = [[item] for item in accepted]
+        accepted.append(variable_greater_score)
+        accepted = [y for x in accepted for y in x] # flatten
 
-        print(f'Melhor variável: {[var_maior_score[0]]} - Score: {valor_maior_score}' )
-        print(aceitas)
-        print()
+        print(f'Round: {step+1}')
+        print(f'Variable selected: {variable_greater_score}')
+        print(f'Score: {greater_score}\n')
         
-    return aceitas
+    return accepted
