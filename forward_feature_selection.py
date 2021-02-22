b8c8b1a1ca3791410d7cac0db6f3b299b8fbcebf
abreukuse <t.kuse6@gmail.com>
2021-02-22 19:52:27 -0300

    Added comments to the function.

--- forward_feature_selection.py
+++ forward_feature_selection.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score, roc_auc_score,
+from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, log_loss,
     mean_absolute_error, mean_squared_error, mean_squared_log_error, median_absolute_error)
 import operator
 
@@ -14,8 +14,33 @@ def forward_feature_selection(X_train,
                               steps=20,
                               greater_is_better=True,
                               **kwargs):
+    """
+    This function performs a variable selection procedure and returns the ones that were selected. 
+    _______________________________________________________________________
+    Parameters:
+
+    {X_train, X_test, y_train, y_test}: pandas dataframe and pandas series as usual.
+
+    model: Single model or a entire sklearn pipeline.
+
+    task_type: {'classification', 'regression'}
+
+    metric: Classification {'accuracy_score', 'precision_score', 'recall_score', 'f1_score', 'roc_auc_score', 'log_loss'}.
+            Regression: {'mean_absolute_error', 'mean_squared_error', 'mean_squared_log_error', 'median_absolute_error'}.
+
+    probability: Boolean. Estimate the y_pred as a float or not. This is required for some classification metrics. Default: False.
+
+    analyse_together: List of lists containing what variables should be considered together in the selection procedure. 
+                      An use case could be a one-hot encoded variable.
+
+    steps: How many rounds should the selection procedure take. Default is 20.
+
+    greater_is_better: For the metric choosed, is it better a greater or lower score? Default is True.
+
+    **kwargs: Key word arguments that should be included for the metric choosed. For instance, 'pos_label' and 'average'.
+    """
     accepted = []
-    greater_score = 0
+    greater_score = 0 if greater_is_better else 1_000_000_000_000
 
     if analyse_together:
         analyse_together_flatten = [item_alone for items_together in analyse_together for item_alone in items_together]
@@ -37,7 +62,8 @@ def forward_feature_selection(X_train,
                                         'precision_score': precision_score,
                                         'recall_score': recall_score,
                                         'f1_score': f1_score,
-                                        'roc_auc_score': roc_auc_score},
+                                        'roc_auc_score': roc_auc_score,
+                                        'log_loss': log_loss},
                                         
                       'regression':{'mean_absolute_error': mean_absolute_error,
                                     'mean_squared_error': mean_squared_error,
@@ -45,9 +71,9 @@ def forward_feature_selection(X_train,
                                     'median_absolute_error': median_absolute_error}
                      }
 
-            parameters = {'y_true': y_test, 'y_pred': y_pred}
+            args = [y_test, y_pred]
 
-            score = scores[task_type][metric](**parameters, **kwargs)
+            score = scores[task_type][metric](*args, **kwargs)
 
             sign = operator.gt if greater_is_better else operator.lt
             if sign(score, greater_score):
